a861e5f717737c9ab432a3b2a220965eac2e51f3
Oleksandr Mytryniuk <o.mytryniuk@mobidev.biz>
2022-11-27 01:16:23 +0200

    Switch to Storyboards

---
+++ LinkFade/Authentication/LoginViewController.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewController.swift
+//  LinkFade
+//
+//  Created by Oleksandr Mytryniuk on 26.11.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        navigationController?.navigationBar.barStyle = .black
+    }
+}
