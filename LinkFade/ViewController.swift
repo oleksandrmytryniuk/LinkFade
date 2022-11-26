225fea5ead4ec67e10e3c48ce79b3895c342f0ca
Oleksandr Mytryniuk <o.mytryniuk@mobidev.biz>
2022-11-26 23:37:44 +0200

    Initial Commit

---
+++ LinkFade/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkFade
+//
+//  Created by Oleksandr Mytryniuk on 26.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
